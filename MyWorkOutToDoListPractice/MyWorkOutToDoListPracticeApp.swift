6b174156e3a4d4eaa8c298a3434414546ba93c48
ShionNakamura <shionnakamura0120@gmail.com>
2024-12-20 15:19:21 +0900

    Initial Commit

---
+++ MyWorkOutToDoListPractice/MyWorkOutToDoListPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkOutToDoListPracticeApp.swift
+//  MyWorkOutToDoListPractice
+//
+//  Created by 仲村士苑 on 2024/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkOutToDoListPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyWorkOutToDoListPracticeTests/MyWorkOutToDoListPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkOutToDoListPracticeTests.swift
+//  MyWorkOutToDoListPracticeTests
+//
+//  Created by 仲村士苑 on 2024/12/20.
+//
+
+import Testing
+@testable import MyWorkOutToDoListPractice
+
+struct MyWorkOutToDoListPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
