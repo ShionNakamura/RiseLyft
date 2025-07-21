258d0782fd1d13754d7b09802611a517c2c97ba4
Shion Nakamura <163878393+ShionNakamura@users.noreply.github.com>
2025-07-21 12:33:04 +0900

    Update MyWorkOutToDoListPracticeTests.swift

--- MyWorkOutToDoListPracticeTests/MyWorkOutToDoListPracticeTests.swift
+++ MyWorkOutToDoListPracticeTests/MyWorkOutToDoListPracticeTests.swift
@@ -12,6 +12,10 @@ struct MyWorkOutToDoListPracticeTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+         func testAddition() {
+        let result = 2 + 3
+        XCTAssertEqual(result, 5)
+    }
     }
 
 }
